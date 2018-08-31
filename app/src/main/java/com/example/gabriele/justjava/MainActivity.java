1517dd5eb06cafa41e5f3aca0e57f61beab1cdf4
Gabriele <g.debartolomeo09@gmail.com>
2018-08-31 13:36:08 +0200

    Added buttons to increment/decrement quantity

--- app/src/main/java/com/example/gabriele/justjava/MainActivity.java
+++ app/src/main/java/com/example/gabriele/justjava/MainActivity.java
@@ -12,19 +12,25 @@ package com.example.gabriele.justjava;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees=2;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees *5);
+
+        //display(quantity);
+        displayPrice(quantity *5);
     }
 
     /**
@@ -42,6 +48,20 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view) {
+        //TextView quantity_text_view = (TextView) findViewById(R.id.quantity_text_view);
+        //int quantity=0;
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view){
+        TextView quantity_text_view = (TextView) findViewById(R.id.quantity_text_view);
+        //int quantity=0;
+        quantity--;
+        display(quantity);
     }
 
 }
